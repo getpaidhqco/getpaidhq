b544302f8288daeb32309084c0696dc90943c073
Meiring de Wet <meiringdewet1@gmail.com>
2025-03-19 12:01:22 +0200

    feat: added default 404

--- internal/lib/request_handler.go
+++ internal/lib/request_handler.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"payloop/internal/application/lib/logger"
 )
 
@@ -13,5 +14,11 @@ type RequestHandler struct {
 // NewRequestHandler creates a new request handler
 func NewRequestHandler(logger logger.Logger) RequestHandler {
 	engine := gin.Default()
+	engine.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"code":    "not_found",
+			"message": "Route not found",
+		})
+	})
 	return RequestHandler{Gin: engine}
 }
