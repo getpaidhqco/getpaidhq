3c4c329c0da3428479742fa9e4796cb0785e6834
mdwt <meiring@checkoutjoy.com>
2025-04-02 10:33:45 +0200

    fix: removed sentry

--- internal/lib/request_handler.go
+++ internal/lib/request_handler.go
@@ -4,7 +4,6 @@ import (
 	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"payloop/internal/api/authn"
 	"payloop/internal/application/lib/logger"
 )
 
@@ -25,17 +24,5 @@ func NewRequestHandler(logger logger.Logger, reporter ErrorReporter) RequestHand
 	engine.Use(sentrygin.New(sentrygin.Options{
 		Repanic: true,
 	}))
-	engine.Use(func(ctx *gin.Context) {
-		user, _ := ctx.Get("user")
-		authUser := user.(authn.User)
-		if hub := sentrygin.GetHubFromContext(ctx); hub != nil {
-			hub.Scope().SetTags(map[string]string{
-				"org_id":  authUser.OrgId,
-				"user_id": authUser.Id,
-				"role":    string(authUser.PrimaryRole),
-			})
-		}
-		ctx.Next()
-	})
 	return RequestHandler{Gin: engine}
 }
