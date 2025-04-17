903da1fb18ce61f872c2b0d2653ad31649e94c09
mdwt <meiring@checkoutjoy.com>
2025-04-17 07:53:20 +0200

    fix: health

--- internal/api/controllers/health_controller.go
+++ internal/api/controllers/health_controller.go
@@ -17,6 +17,5 @@ func NewHealthController(logger logger.Logger) HealthController {
 }
 
 func (u HealthController) Healthcheck(c *gin.Context) {
-	panic("simulated panic for testing")
 	c.JSON(200, map[string]string{"status": "ök"})
 }
